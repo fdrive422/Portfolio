ec2a8900422e5bd558cb9d2d0a465c95ffc120be
fdrive422 <fafrago@gmail.com>
2022-08-31 19:34:54 -0700

    addy update

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -23,7 +23,7 @@ const Footer = ({ data }) => {
             <li>
               Made by{" "}
               <a title="FDrive" href="http://www.fdrive.com/">
-                KIKO
+                fdrive
               </a>
             </li>
           </ul>
